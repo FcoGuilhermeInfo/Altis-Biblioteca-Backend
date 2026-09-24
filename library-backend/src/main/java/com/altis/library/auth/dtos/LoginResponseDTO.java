7867bf6cfa3499f08ebdd923d4163c3d419fda1c
FcoGuilhermeInfo <fcoguilherme12@gmail.com>
2026-09-24 16:19:56 -0300

    feat: add user id in login response

--- library-backend/src/main/java/com/altis/library/auth/dtos/LoginResponseDTO.java
+++ library-backend/src/main/java/com/altis/library/auth/dtos/LoginResponseDTO.java
@@ -1,6 +1,9 @@
 package com.altis.library.auth.dtos;
 
+import java.util.UUID;
+
 public record LoginResponseDTO(
+        UUID user_id,
         String token,
         String type,
         long expiresIn
